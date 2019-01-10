dab859d4d8e7aae3189b4e11330d605e063a7461
lionelnanchen <lionel.nanchen@heig-vd.ch>
2019-01-10 15:44:40 +0100

    delete image

---
+++ build/precache-manifest.f3cb2c07c951665bb79e1253eedd9ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23353088035749cd5eb7",
+    "url": "/static/js/main.23353088.chunk.js"
+  },
+  {
+    "revision": "1af69302d229ab7e12e9",
+    "url": "/static/js/1.1af69302.chunk.js"
+  },
+  {
+    "revision": "23353088035749cd5eb7",
+    "url": "/static/css/main.2e33185d.chunk.css"
+  },
+  {
+    "revision": "cb3dbc84d2b63e8b0872e432d275365a",
+    "url": "/index.html"
+  }
+];
